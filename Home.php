402f1a29dea890161f8c43052a439f7bc0e9c948
eriklaguliga <sabarorang@gmail.com>
2017-09-09 15:44:17 +0700

    Create Home.php

---
+++ Home.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link href="css/bootstrap.css" rel="stylesheet">
+	<link href="css/carousel.css" rel="stylesheet">
+	<link href="css/style.css" rel="stylesheet">
+	<script src="js/jquery-3.2.1.js"></script>
+	<script src="js/bootstrap.js"></script>
+	<link rel="icon" href="img/buku.png">
+	<title>Inventaris Telkom</title>
+</head>
+<body>
+
+	<nav class="navbar navbar-inverse navbar-fixed-top">
+	<div class="container">
+		<div class="navbar-header">
+		<button type="button" class="navbar-toggle
+		collapsed" data-toggle="collapse" data-target="#navbar">
+			<span class ="icon-bar"></span>
+			<span class ="icon-bar"></span>
+			<span class ="icon-bar"></span>
+			<span class ="icon-bar"></span>
+			<span class ="icon-bar"></span>
+			<span class ="icon-bar"></span>			
+		</button>
+
+			<a class="navbar-brand" href="index.php">E - inventaris</a>	
+		</div>
+		<div id="navbar" class ="navbar-collapse collapse">
+			<ul class="nav navbar-nav">
+				<li><a href="home">Home</a></li>
+				<li><a href="#about">About</a></li>
+				<li><a href="registrasi.php">Registrasi</a></li>
+				<li><a href="login.php">Log in</a></li>
+				<li><a href="cekgedung.php">Log In</a></li>
+				<li><a href="bookgedung.php">Book Inventaris</a></li>
+			</ul>
+			
+		</div>
+	</div>	
+</nav>
+
+<!--ini Home-->
+<div id="Home">
+	<img src="img/telkomuniversity2.png">
+</div>
+
+<!-- ini about-->
+
+<div id="About">
+	<div class="container marketing">
+
+		<div class="col-lg-4">
+			<img class = "img-circle" src="img/logo.png"
+			width="140" height="140">
+			<h2>About</h2>
+			<p>Website ini berguna untuk memudahkan para mahasiwa,dosen,pegawai,atau instansi tertentu untuk melaukan peminjaman inventaris di Telkom University.</p>
+		</div>
+	</div>
+</div>
+
+
+
+	
+
+</body>
+</html>
