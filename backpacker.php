f486d06a1efdf898a903bf1be7e88c88e55fb7b3
eriklaguliga <sabarorang@gmail.com>
2017-11-08 19:37:34 +0700

    Create backpacker.php

---
+++ backpacker.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>E-inventaris</title>
+</head>
+
+<body>
+
+	<link href="css/bootstrap.css" rel="stylesheet">
+	<link href="css/carousel.css" rel="stylesheet">
+	<link href="css/style.css" rel="stylesheet">
+	<script src="js/jquery-3.2.1.js"></script>
+	<script src="js/bootstrap.js"></script>
+	<link rel="icon" href="img/passport.png">
+
+	<nav class="navbar navbar-inverse navbar-fixed-top">
+	<div class="container">
+		<div class="navbar-header">
+		<button type="button" class="navbar-toggle
+		collapsed" data-toggle="collapse" data-target="#navbar">
+			<span class ="icon-bar"></span>
+			<span class ="icon-bar"></span>
+			<span class ="icon-bar"></span>
+			<span class ="icon-bar"></span>
+			<span class ="icon-bar"></span>
+			<span class ="icon-bar"></span>				
+		</button>
+			<a class="navbar-brand" href="index.php">E-Inventaris</a>
+		</div>
+		<div id="navbar" class ="navbar-collapse collapse">
+			<ul class="nav navbar-nav">
+					<li><a href="#home">Home</a></li>
+				<li><a href="#about">Book</a></li>
+				<li><a href="#program">About</a></li>
+				<li><a href="#contact">Contact</a></li>
+				<li><a href="signup.php">Registrasi
+				<li><a href="masuk.php">Log In</a></li>
+			</ul>
+		</div>
+	</div>	
+	</nav>
+
+<div id="home">
+	<div class="container marketing">
+		<div class ="row">
+			<div class="col-lg-12">
+				<br>
+				<br>
+				<br>
+				<br>
+				<br>
+				<center>
+				<img class ="img-circle" src="img/gedung.png"
+				width="140" height="140">
+				<h2>Gedung dan Ruangan</h2>
+				<p>Menyediakan berbagi program Peminjaman untuk peminjaman gedung dan runagan di Tel-U Seperti gedung GSG,gedung TUCH,ruangan e302,dan masih banyak lagi..</p>
+				<p><a class="btn btn-default" href="camping.php">Next &raquo;</a></p>
+				</center>
+			</div>
+		</div>	
+	</div>
+</div>
+
+</body>
+</html>
